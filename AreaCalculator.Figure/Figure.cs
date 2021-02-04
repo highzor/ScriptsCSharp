e5324c6ddad54a33bca62fc811638f638f5ab501
bnmmnb1337@gmail.com <bnmmnb1337@gmail.com>
2021-02-04 18:48:56 +0300

    no message

---
+++ AreaCalculator.Figure/Figure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator.Figure
+{
+    public abstract class Figure
+    {
+        /// <summary>
+        /// Объявляем поле при необходимости
+        /// </summary>
+        private readonly Lazy<double> area;
+        /// <summary>
+        /// true\false наличия прямого угла
+        /// </summary>
+        public double Area => area.Value;
+        /// <summary>
+        /// Конструктор с площадью при необходимости
+        /// </summary>
+        protected Figure()
+        {
+            area = new Lazy<double>(CalculateArea);
+        }
+        /// <summary>
+        /// Расчет площади
+        /// </summary>
+        /// <returns></returns>
+        protected abstract double CalculateArea();
+    }
+}
