3452b5a4e294b720207204758a696909da43861d
highzor <52636901+highzor@users.noreply.github.com>
2021-02-04 18:53:02 +0300

    Delete ProductCategory.sql

--- ProductCategory.sql
+++
@@ -1,4 +0,0 @@
-SELECT Products.productName, Category.categoryName
-FROM     Category RIGHT OUTER JOIN
-                  ProductCategory ON Category.ID_Category = ProductCategory.ID_Category INNER JOIN
-                  Products ON ProductCategory.ID_Product = Products.ID_product
